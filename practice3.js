fcf1ac3531336e21a4e1db9551afe7ae9c523eab
Rkv1650 <rkvgaming9@gmail.com>
2023-09-19 01:06:35 +0530

    pract

---
+++ practice3.js
@@ -0,0 +1,8 @@
+// write a program to print of a student in an object using for loop
+//obj ={monty:89,rahul:90,ram:67,aakas:67}
+
+let obj={
+    monty:89,rahul:90,ram:67,aakas:67};
+for(let a in obj){
+    console.log(a + "is marks " + obj[a] ) 
+}
