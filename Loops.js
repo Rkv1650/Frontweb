5b42140a4f187201cd19c8b673974b2164e376e8
Rkv1650 <rkvgaming9@gmail.com>
2023-09-18 21:22:45 +0530

    forloop

---
+++ Loops.js
@@ -0,0 +1,12 @@
+// loops >in witch a code again to again  execute code
+// for loop >
+// it is used to fixed time execute code 
+// while loop->in which a code execute when one condition is not true 
+// do while loop-> minimam one time code execute 
+
+// Q1> use for loop and print 1 to 100 numbers
+
+for(let i=1; i<=100; i++){
+    console.log(i)
+};
+
