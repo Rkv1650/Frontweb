efb81fdf693ae88c3fe6bee8a8c4d269107be85e
Rkv1650 <rkvgaming9@gmail.com>
2023-09-20 10:18:13 +0530

    math

--- practice3.js
+++ practice3.js
@@ -5,4 +5,8 @@ let obj={
     monty:89,rahul:90,ram:67,aakas:67};
 for(let a in obj){
     console.log(a + "is marks " + obj[a] ) 
+};
+const mean=(a,b,c,d,e)=>{
+    return(a+b+c+d)%4
 }
+console.log(mean(4,5,6,7))
