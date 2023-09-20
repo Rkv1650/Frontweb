d4133e1187c29c7dc56f735eb9857e6759cc3eff
Rkv1650 <rkvgaming9@gmail.com>
2023-09-20 15:53:48 +0530

    j

--- forlinoop.js
+++ forlinoop.js
@@ -8,3 +8,15 @@ for(let a in student_marks){
     console.log(a + ' :' + student_marks[a])
 }
 console.log("Hello")
+var num=1;
+while (num<=10) {
+    document.write("Hi Rahul")
+    num++
+}
+
+const a=10;
+let i=0;
+do{
+    console.log(i)
+    i++
+}while (i<a) 
---
+++ function.js
@@ -0,0 +1,5 @@
+// Function --> a set or collection of code or statement or Intersection  which are used to particuler action task
+const sum =(a,b)=>{
+    return (a+b)/2*8;
+};
+console.log(sum(6,8))
