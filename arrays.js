739df5a5062958f906b3ee7f4195ac74525ead4d
Rkv1650 <rkvgaming9@gmail.com>
2023-09-21 12:12:57 +0530

    jk

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var a=[10,2,35,16,5,12];
+var b=[1,2,3,4,5,6,7];
+var c=[101,102,103,104]
+let c=a.join(",")
+console.log(c)
+a.pop()
+console.log(a)
+a.push(7)
+console.log(a)
+a.shift()
+console.log(a)
+a.unshift(90)
+console.log(a)
+var sum=a.concat(b,c);
+console.log(sum)
+
---
+++ stringjs.js
@@ -0,0 +1,9 @@
+// string => string is inmutable
+const num="Rahul Kumar";
+console.log(num.length);
+console.log(num.toLocaleUpperCase());
+console.log(num.toLocaleLowerCase());
+console.log(num.slice(2,5))
+var nummber=[1,3,5,7,6]
+var b=nummber.toString()
+console.log(typeof(b))
