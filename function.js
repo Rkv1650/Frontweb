2f16964ec95941583cc6dfccdb0ef0f12c7be1c2
Rkv1650 <rkvgaming9@gmail.com>
2023-09-21 10:16:23 +0530

    op

--- function.js
+++ function.js
@@ -1,5 +1,19 @@
 // Function --> a set or collection of code or statement or Intersection  which are used to particuler action task
 const sum =(a,b)=>{
-    return (a+b)/2*8;
+    return a+b;
+    
+}
+console.log(sum(2,5))
+//Q2 write a function to find mean of 5 numbers
+const total =(a,b,c,d,e)=>{
+    return (a+b+c+d+e)/2;
 };
-console.log(sum(6,8))
+console.log(total(8,9,7,60,20))
+const marks=()=>{
+    let num=parseInt(prompt("Enter A Value "));
+    let num1=parseInt(prompt("Enter A Value "));
+    const total=num +num1
+    console.log(total)
+
+};
+console.log(marks())
